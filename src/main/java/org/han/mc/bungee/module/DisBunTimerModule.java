d8a826490feccea801774e302797c7389ceedd91
Hanro50 <hlareman2@gmail.com>
2020-08-15 10:53:30 +0200

    Restructure

--- src/main/java/org/han/mc/bungee/module/DisBunTimerModule.java
+++
@@ -1,9 +0,0 @@
-package org.han.mc.bungee.module;
-
-import java.util.Timer;
-
-public abstract class DisBunTimerModule extends DisBunModule {
-	public abstract int DefaultTime();
-	public Timer timer = null;
-
-}
--- src/main/java/org/han/xlib/types/vectors/vectors.java
+++
@@ -1,7 +0,0 @@
-package org.han.xlib.types.vectors;
-import java.util.List;
-
-public interface vectors<type> {
-	public List<type> get();
-	
-}
